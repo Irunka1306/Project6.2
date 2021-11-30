5f88e1842aa99af6421925ab6444c8422f7961a3
Irunka1306 <92487724+Irunka1306@users.noreply.github.com>
2021-11-30 14:42:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace lab6._2
+{
+    public class Program
+    {
+
+        public static int Sum(int[] Arr)
+        {
+            return Arr.Min() + Arr.Max();
+        }
+        public static void Main(string[] args)
+        {
+            int count;
+            Random random = new Random();
+
+            Console.Write("Enter count of elements = ");
+            count = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[count];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(100);
+
+                Console.Write(array[i] + "\t");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Result = " + Sum(array));
+
+        }
+    }
+}
+    
+
+
